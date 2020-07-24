03143d7c05c5219101b1bda3de3faf386e5d0eec
MD JAHID HASAN <tj.tusher@gmail.com>
2020-07-24 22:33:55 +0600

    after api integration

--- weatherlookup/views.py
+++ weatherlookup/views.py
@@ -3,9 +3,20 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+    api_request = requests.get("http://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=94102&date=2020-07-23&distance=25&API_KEY=445AE684-7031-4F15-96D7-F237990B9E38")
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "error..."
+
+    if api[0]['Category']['Name'] == "Good":
+
+        category_description = "(0 to 50)Air quality is satisfactory, and air pollution poses little or no risk."
+
+    return render(request, 'home.html', {'api': api})
 
 
 def about(request):
-    return render(request, 'about.html', {} )
-
+    return render(request, 'about.html', {})
